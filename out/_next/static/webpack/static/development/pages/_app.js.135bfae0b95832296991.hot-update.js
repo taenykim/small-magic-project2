b08ec1782603f4e9f7385a70053ca8f7dc3a7d98
taeeun kim <kimtaeeeny@gmail.com>
2020-04-22 14:19:58 +0900

    deploy to gh-pages

---
+++ out/_next/static/webpack/static/development/pages/_app.js.135bfae0b95832296991.hot-update.js
@@ -0,0 +1,92 @@
+webpackHotUpdate("static/development/pages/_app.js",{
+
+/***/ "./components/Header.js":
+/*!******************************!*\
+  !*** ./components/Header.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+var _this = undefined,
+    _jsxFileName = "/Users/taeeunkim/Desktop/taenykim/small-magic-project/small-magic-project2/components/Header.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var HeaderContainer = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].div.withConfig({
+  displayName: "Header__HeaderContainer",
+  componentId: "o4x68b-0"
+})(["display:flex;position:fixed;top:0;left:50%;transform:transition(-50%,0);margin-top:10px;z-index:5;font-size:12px;font-family:escore6;color:#666;& > a{text-decoration:none;color:#666;}"]);
+var FullscreenText = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].p.withConfig({
+  displayName: "Header__FullscreenText",
+  componentId: "o4x68b-1"
+})(["cursor:pointer;"]);
+var NoteText = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].p.withConfig({
+  displayName: "Header__NoteText",
+  componentId: "o4x68b-2"
+})(["cursor:pointer;"]);
+
+var Header = function Header() {
+  var _useState = Object(react__WEBPACK_IMPORTED_MODULE_0__["useState"])(false),
+      full = _useState[0],
+      setFull = _useState[1];
+
+  Object(react__WEBPACK_IMPORTED_MODULE_0__["useEffect"])(function () {
+    document.addEventListener('fullscreenchange', function () {
+      setFull(!full);
+    });
+  });
+
+  var openFullScreen = function openFullScreen() {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
+  };
+
+  return __jsx(HeaderContainer, {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 47,
+      columnNumber: 5
+    }
+  }, __jsx(FullscreenText, {
+    onClick: openFullScreen,
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 48,
+      columnNumber: 7
+    }
+  }, full ? 'exit full screen' : 'full screen'), "\xA0\xA0|\xA0\xA0", __jsx("a", {
+    href: "https://taeny.dev/project/%EC%86%8C%EB%A7%88%EB%B2%95-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B81/",
+    target: "_blank",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 52,
+      columnNumber: 7
+    }
+  }, __jsx(NoteText, {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 56,
+      columnNumber: 9
+    }
+  }, "note")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Header);
+
+/***/ })
+
+})
+//# sourceMappingURL=_app.js.135bfae0b95832296991.hot-update.js.map
