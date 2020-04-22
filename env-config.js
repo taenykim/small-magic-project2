0685af6f77c4f61717e5e2e6d7eb3c349c320960
taeeun kim <kimtaeeeny@gmail.com>
2020-04-22 16:34:15 +0900

    netlify test

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
-const prod = process.env.NODE_ENV === 'production'
+// const prod = process.env.NODE_ENV === 'production'
 
-module.exports = {
-  'process.env.BACKEND_URL': prod ? '/small-magic-project2' : '',
-}
+// module.exports = {
+//   'process.env.BACKEND_URL': prod ? '/small-magic-project2' : '',
+// }
